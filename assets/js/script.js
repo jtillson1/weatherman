31a4457a2c078c24ba92106797fbf92198d79679
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-07-13 14:26:28 -0500

    current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,21 +40,7 @@ else {
          }
      });
  };
- //forecast for the week
- var getForecast = function(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=263c76e2bd90911c40bad2b37dc3d48c&units=imperial";
 
-    fetch(apiUrl).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data){
-               displayForecast(data , cityName);
-            });
-            else {
-                alert("Uh Oh: " response.statusText);
-            }
-        }
-    });
- };
 //display the data for the current weather (top right section)
  var displayWeather = function(info, cityName) {
     var cityHeadingEl = document.createElement("h3");
@@ -100,3 +86,104 @@ else {
         };
     };
  };
+//end of current weather container 
+//begin forecast 
+ var getForecast = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=263c76e2bd90911c40bad2b37dc3d48c&units=imperial";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data){
+               displayForecast(data , cityName);
+            });
+            else {
+                alert("Uh Oh: " response.statusText);
+            }
+        }
+    });
+ };
+var displayForecast = function(info) {
+    var firstDay = (currentDate.getMonth()+1)+'/'+(currentDate.getDate()+1)+'/'+currentDate.getFullYear();
+    var secondDay = (currentDate.getMonth()+1)+'/'+(currentDate.getDate()+2)+'/'+currentDate.getFullYear();
+    var thirdDay = (currentDate.getMonth()+1)+'/'+(currentDate.getDate()+3)+'/'+currentDate.getFullYear();
+    var fourthDay = (currentDate.getMonth()+1)+'/'+(currentDate.getDate()+4)+'/'+currentDate.getFullYear();
+    var fifthDay = (currentDate.getMonth()+1)+'/'+(currentDate.getDate()+5)+'/'+currentDate.getFullYear();
+
+    var dataArr = [
+        {
+            day: firstDay,
+            icon: info.list[4].weather[0].icon,
+            temp: info.list[4].main.temp,
+            hum: info.list[4].main.humidity
+        },
+        {
+            day: secondDay,
+            icon: info.list[12].weather[0].icon,
+            temp: info.list[12].main.temp,
+            hum: info.list[12].main.humidity
+        },
+        {
+            day: thirdDay,
+            icon: info.list[20].weather[0].icon,
+            temp: info.list[20].main.temp,
+            hum: info.list[20].main.humidity
+        },
+        {
+            day: fourthDay,
+            icon: info.list[28].weather[0].icon,
+            temp: info.list[28].main.temp,
+            hum: info.list[28].main.humidity
+        },
+        {
+            day: fifthDay,
+            icon: info.list[36].weather[0].icon,
+            temp: info.list[36].main.temp,
+            hum: info.list[36].main.humidity
+        }
+    ];
+    //remove everything before adding new forecast cards
+    forecastContainer.textContent = "";
+    forecastHeading.textContent = "";
+    //create forecast heading
+    var forecastHeading = document.createElement("h3");
+    forecastHeading.textContent = "5-Day Forecast";
+
+    forecastContainer.appendChild(forecastHeading);
+
+    //loop for forecast cards
+
+    for (i = 0; i < dataArr.length; i++) {
+        //create forecast card 
+        var cardEl = document.createElement("div");
+        cardEl.classList.add("card", "text-light", "bg-primary", "m-1");
+
+        //create card info 
+        var cardBodyEl = document.createElement("div");
+        cardBodyEl.classList.add("card-body", "p-1");
+
+        var cardTitleEl = document.createElement("h5");
+        cardTitleEl.textContent = dataArr[i].day;
+
+        cardBodyEl.appendChild(cardTitleEl);
+
+        //icon img 
+        var cardIconEl = document.createElement("img");
+        var iconUrl = "https://openweathermap.org/img/wn/" + dataArr[i].icon + ".png"
+        cardIconEl.setAttribute("src", iconUrl);
+
+        cardBodyEl.appendChild(cardIconEl);
+
+        //create forecadst card temperatures
+        var cardTempEl = document.createElement("p");
+        cardTempEl.classList.add("card-text");
+        cardTempEl.textContent = "Temp: " + dataArr[i].temp + "\u00B0";
+
+        cardBodyEl.appendChild(cardTempEl);
+
+
+
+
+
+    }
+
+}
